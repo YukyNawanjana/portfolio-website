942b83c52a4d65857409446924fa82a28b5cb196
YukyNawanjana <yasirunawanjana70@gmail.com>
2021-07-01 12:43:28 +0530

    update web

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,38 @@
-import "./intro.scss"
+import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
-export default function intro() {
+export default function Intro() {
+
+    const textRef = useRef();
+
+    useEffect(()=>{
+        init(textRef.current,{
+            showCursor: false,
+            backDelay: 1500, 
+            showCursor: true,
+            strings: ["Developer", "Designer", "Content Creator"],
+        });
+    },[]);
+
     return (
         <div className="intro" id="intro">
-           
+           <div className="left">
+               <div className="imgContainer">
+                   <img src="assets/man.png" alt="" />
+               </div>
+           </div>
+           <div className="right">
+               <div className="wrapper">
+                   <h2>Hi There, I'm</h2>
+                   <h1>Yuky Nawanjana</h1>
+                   <h3>Freelancer <span ref={textRef}></span></h3>
+               </div>
+               <a href="#portfolio">
+                   <img src="assets/down.png" alt="" />
+               </a>
+           </div>
         </div>
     )
 }
+ 
